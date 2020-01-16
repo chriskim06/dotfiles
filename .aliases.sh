d43f4a6183ffc4a59e17c5688b15ba1772986ada
Chris Kim <chriskim06@gmail.com>
2020-01-16 09:11:48 -0800

    Add an alias for running krew tests

--- .aliases.sh
+++ .aliases.sh
@@ -29,5 +29,6 @@ alias prune='docker system prune -af'
 alias tree='tree -a -I "\.git|node_modules"'
 alias kc='kubectl'
 alias kdev='KREW_ROOT="/home/kimc/playground" /home/kimc/go/src/sigs.k8s.io/krew/out/bin/krew-linux_amd64'
+alias tkrew='go clean -testcache && hack/make-binary.sh && hack/run-integration-tests.sh && hack/run-tests.sh'
 complete -F __start_kubectl kc
 [[ -f ~/.private ]] && source ~/.private
