5393693787ad23eb153fa8cd6e210254380ef9fd
Chris Kim <chriskim06@gmail.com>
2023-03-14 07:03:04 -0700

    update

--- .fzf-stuff.sh
+++ .fzf-stuff.sh
@@ -21,21 +21,22 @@ fstash() {
     git stash list --pretty="%C(yellow)%h %>(14)%Cgreen%cr %C(blue)%gs" |
       fzf --ansi --no-sort --query="$q")
   do
-    read -a out <<< $out
+    read -r -a out <<< "$out"
     sha="${out[0]}"
     sha="${sha%% *}"
     [[ -z "$sha" ]] && continue
-    git stash show -p $sha
+    git stash show -p "$sha"
   done
 }
 
 fshow() {
-  _gitLogLineToHash="echo {} | grep -o '[a-f0-9]\{7\}' | head -1"
-  _viewGitLogLine="$_gitLogLineToHash | xargs -I % sh -c 'git show --color=always % | diff-so-fancy'"
+  _viewGitLogLine="echo {} | grep -o '[a-f0-9]\{7\}' | head -1 | xargs -I % sh -c 'git show --color=always % | delta'"
   git log --color=always --format="%C(bold red)%h%C(reset) %C(bold cyan)<%ar> %C(green)%an%C(reset)%C(bold yellow)%d%C(reset) %C(white)%s%C(reset)" "$@" |
     fzf --no-sort --reverse --tiebreak=index --no-multi --no-mouse \
     --ansi --preview="$_viewGitLogLine" \
-    --bind "enter:execute:$_viewGitLogLine | less -R"
+    --bind "enter:execute:$_viewGitLogLine | less -R" \
+    --bind "J:preview-down" \
+    --bind "K:preview-up"
 }
 
 complete -F _fzf_path_completion -o default -o bashdefault vim
